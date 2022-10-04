d5c460fb53d81aec374202afb7317ace8fad1fa7
DiasKarol <karol.gulbarg@gmail.com>
2022-10-04 11:31:59 -0300

    front 01: home/cadastro/login/store

---
+++ src/public/module/js/login.js
@@ -0,0 +1,23 @@
+export default () => {
+    const conteiner = document.createElement('section');
+
+    const conteudo = `
+    <nav id="navLogin">
+        <a id="registerAnchorButton" data-pathname="/cadastro">Cadastrar</a>
+        <a id="LoginAnchorButton" data-pathname="/login" class="selected">Login</a>
+    </nav>
+
+    <div id="conteinerLogin">
+        <div id="userDoll">
+        
+        </div>
+        <input type="text" id="emailLogin" maxlength="80" placeholder="E-mail" required tabindex="1">
+        <input type="password" id="passwordLogin" maxlength="10" placeholder="Senha" required tabindex="2">
+        <button id="btnLogin">Entrar</button>
+    </div>
+    `;
+
+    conteiner.innerHTML = conteudo;
+
+    return conteiner;
+}
---
+++ src/public/module/js/store.js
@@ -0,0 +1,66 @@
+export default () => {
+    const conteiner = document.createElement('section');
+
+    conteiner.style.cssText = `display: grid;
+    grid-template-columns: 35% 65%;
+    justify-content: center;
+    align-items: center;
+    gap: 20px
+    `;
+
+    const conteudo = `
+
+        <section id="spacUser">
+                <h2 id="nameUser">Maggie</h2>
+                <div id="avatar"></div>
+                <p id="tempoMemeiro">Memeiro desde Setembro de 2022</p>
+                <div id="bar"></div>
+                <p>Minha carteira:</p>
+                <div id="pocket">R$00,00</div>
+        </section>
+
+        <div id="conteinerStore">
+
+            <section id="bonus">
+                <h3>Bônus diário</h3>
+                <div id="loginBonus"></div>
+            </section>
+
+
+            <section id="storeFig">
+            <h3>Loja de figurinhas</h3>
+
+            <div class="conteinerPack">
+                <img src="./module/img/comum.svg" alt="pacote de figurinhas normais">
+                <p class="less">-</p>
+                <input type="text" placeholder="0">
+                <p class="more">+</p>
+                <button class="btnPay">Adicionar</button>
+            </div>
+
+            <div class="conteinerPack">
+                <img src="./module/img/rara.svg" alt="pacote de figurinhas normais">
+                <p class="less">-</p>
+                <input type="text" placeholder="0">
+                <p class="more">+</p>
+                <button class="btnPay">Adicionar</button>
+            </div>
+
+            <div class="conteinerPack">
+                <img src="./module/img/gold.svg" alt="pacotes de figurinhas gold">
+                <p class="less">-</p>
+                <input type="text" placeholder="0">
+                <p class="more">+</p>
+                <button class="btnPay">Adicionar</button>
+            </div>
+
+            <button id="cartBtn"></button>
+
+            </section>
+        </div>
+    `;
+
+    conteiner.innerHTML = conteudo;
+
+    return conteiner;
+}
