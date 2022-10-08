e85e0c08dfd3c341b687ee80a0f20fbebb7e2fa4
Scytar <scytarnoob@gmail.com>
2022-10-08 09:36:09 -0300

    Added store design to deposit value in wallet

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# coleciome-me
+# Coleciome-me
+
+This is an academic project required by [Alpha EdTech](https://www.alphaedtech.org.br) developed by
+### *[Ana Karolina de Oliveira Dias](https://github.com/DiasKarol)*
+### *[Bruno Rocha Adami da Silva](https://github.com/Scytar)*
+### *[Carlos Eduardo Lima Oliveira](https://github.com/Oliveira-Carlos)*
+### *[Luiz Antônio Marques Junior](https://github.com/LuizMqs)*
+
+The goal consists in developing a Web Application for collecting and trading of items.
+
+`Requirements:`
+- Use of API RESTful,
+- SPA (Single Page Application),
+- VanillaJS focused,
+- Session Control,
+- Image upload, storage and display
+
+*This project was required in September 19th*
--- src/public/module/js/tradingMyOffers.js
+++ src/public/module/js/tradingMyOffers.js
@@ -9,14 +9,33 @@ export default () => {
     </nav>
 
     <div id="containerTrading">
-        <section class="conteinerTrading">
-            <img src="./module/img/home2.png">
-            <spam><?xml version="1.0" ?><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title/><g data-name="Layer 2" id="Layer_2"><polygon points="18.71 18.71 22.41 15 18.71 11.29 17.29 12.71 18.59 14 10 14 10 16 18.59 16 17.29 17.29 18.71 18.71"/><polygon points="6.71 11.29 5.41 10 14 10 14 8 5.41 8 6.71 6.71 5.29 5.29 1.59 9 5.29 12.71 6.71 11.29"/></g></svg></spam>
-            <img src="./module/img/home4.png">   
-        </section>
+    <div class="cardsGroup">
+    <div class="cardsTrading">
+
     </div>
 
-    <button id="solicitar">Outras ofertas</button>
+    <div id="linha-vertical"></div>
+
+    <div class="cardsTrading">
+
+    </div>
+</div>
+
+<div id="linha-horizontal"></div>
+
+<div class="cardsGroup"> 
+    <div class="cardsTrading">
+
+    </div>
+
+    <div id="linha-vertical"></div>
+
+    <div class="cardsTrading">
+    </div>
+
+</div>
+    </div>
+
     `
 
     exibe.innerHTML = conteudo;
