b06ba5816bb3c2744bf41383bc7bda709468cd89
DiasKarol <karol.gulbarg@gmail.com>
2022-10-09 16:26:27 -0300

    add logout bottun

--- src/public/module/js/registration.js
+++ src/public/module/js/registration.js
@@ -1,5 +1,6 @@
 export default () => {
     const conteiner = document.createElement('section')
+    conteiner.setAttribute("id", "sectionFather")
 
     const conteudo = `
     <section class="cadastrogin">
@@ -10,20 +11,15 @@ export default () => {
 
         <div id="conteinerLogin">
             <div id="userDollRegister"></div>
-            <select name="options" id="dolls" required>
-                <option value="01">menino I</option>
-                <option value="02">menino II</option>
-                <option value="03">menino III</option>
-                <option value="04">Menino IV</option>
-                <option value="05">Menina I</option>
-                <option value="06">Menina II</option>
-                <option value="07">Menina III</option>
-                <option value="08">Menina IV</option>
-            </select>
+            
             <input type="text" id="nameRegister" maxlength="80" placeholder="Nome" required tabindex="1">
-            <input type="text" id="emailRegister" maxlength="80" placeholder="E-mail" required tabindex="2">
+
+            <input type="email" id="emailRegister" maxlength="80" placeholder="E-mail" required tabindex="2">
+
             <input type="password" id="passwordRegister" maxlength="10" placeholder="Senha" required tabindex="3">
+
             <input type="password" id="passwordVerifyRegister" maxlength="10" placeholder="Confirme sua senha" required tabindex="4">
+
             <button id="btnRegiter">Registrar</button>
         </div>
     </section>
