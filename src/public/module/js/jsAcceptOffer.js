5092b5d70a1ba346ef7e23a314c2f80e7daedc58
Scytar <scytarnoob@gmail.com>
2022-10-14 12:25:06 -0300

    Fix display of message errors when rejecting and accepting trades answers (Tested)

--- src/public/module/js/jsAcceptOffer.js
+++ src/public/module/js/jsAcceptOffer.js
@@ -3,9 +3,9 @@ import getMyOffers from './jsGetMyOffers.js'
 
 export default function acceptOffer(dataToAccept){
 
-    const sectionCollection = document.getElementById('sectionCollection');
+    const sectionCollection = document.getElementById('sectionColection');
 
-    let loading = true
+    // let loading = true
 
     const options = {
         method:'put',
@@ -23,15 +23,24 @@ export default function acceptOffer(dataToAccept){
     fetch('/trades/accept_card_offer' , options)
     .then(res=>res.json())
     .then(data=>{
-        loading = false
+        // loading = false
 
-        renderModal(data.message)
-        renderSection(sectionCollection)
-        getMyOffers()
+        console.log(data)
 
+        if (data.message.severity != "ERROR"){
+
+            renderModal(data.message)
+            renderSection(sectionCollection)
+            getMyOffers();
+        } else {
+            renderModal(`Ops! Parece que alguém não tem 🍪 BitCookies o suficiente!`)
+            renderSection(sectionCollection)
+            getMyOffers();
+        }
     })
     .catch(err=>{
-        loading = false
+        // loading = false
+        // console.log(err)
         renderModal(err)
     })
 
